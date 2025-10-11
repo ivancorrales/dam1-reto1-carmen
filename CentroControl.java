a7a2ce6d8b584312f610f2fdb3a17ec79106d842
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2025-10-11 14:28:37 +0200

    Update CentroControl.java

--- CentroControl.java
+++ CentroControl.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class CentroControl {
     static Scanner sc = new Scanner(System.in);
     public static void mostrarSaludo(){
-        System.out.print("Introduce tu nombre/apodo: ");
+        System.out.printf("%nIntroduce tu nombre/apodo: ");
         String nombre = sc.nextLine();
         System.out.printf("Hola, bienvenid@ %s", nombre);
     }
     public static void calcularDescuento(){
         final double DESCUENTO = 0.1;
-        System.out.print("Introduce el precio de un producto: ");
+        System.out.printf("%nIntroduce el precio de un producto: ");
         double precio = sc.nextDouble();
         double precioDescuentado = precio * (1 - DESCUENTO);
         System.out.printf("El precio descontado es: %.2f €", precioDescuentado);
     }
     public static void evaluarTemperatura(){
-        System.out.print("¿Qué temperatura hace? ");
+        System.out.printf("%n¿Qué temperatura hace? ");
         double temperatura = sc.nextDouble();
         if(temperatura<=15){
             System.out.println("Frío");
@@ -27,11 +27,16 @@ public class CentroControl {
             System.out.println("Calor");
         }
     }
+    public static void mostarResumen(){
+        System.out.printf("%nIntroduce tu nombre: ");
+        String nombre = sc.nextLine();
+        System.out.printf("Hola %s hoy es 11/10/2025", nombre);
+    }
     public static void main(String[] args) {
-        System.out.printf("=== CENTRO DE CONTROL ===%n1. Mostrar saludo%n2. Calcular descuento%n3. Evaluar temperatura%nElige una opción (1-3): ");
+        System.out.printf("=== CENTRO DE CONTROL ===%n1. Mostrar saludo%n2. Calcular descuento%n3. Evaluar temperatura%n4. Mostrar Resumen%nElige una opción (1-4): ");
         int opcion = sc.nextInt();
         sc.nextLine();
-        if ((opcion == 1 || opcion == 2 || opcion == 3)){
+        if ((opcion == 1 || opcion == 2 || opcion == 3 || opcion == 4)){
             if(opcion == 1){
                 mostrarSaludo();
             }
@@ -40,6 +45,9 @@ public class CentroControl {
             }
             if(opcion == 3){
                 evaluarTemperatura();
+            }
+            if(opcion == 4){
+                mostarResumen();
             }
         }else{
             System.out.println("Opción no válida");
