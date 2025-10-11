09a214ef83822f9f98dbd6fd89cab163fd9eb331
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2025-10-11 14:28:48 +0200

    Update EdadRestante.java

--- EdadRestante.java
+++ EdadRestante.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class EdadRestante{
-    public static int Hasta100(int edad){
+    public static int hasta100(int edad){
         return 100-edad;
     }
-    public static int Desde100(int edad){
+    public static int desde100(int edad){
         return edad-100;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduce tu edad: ");
         int edad = sc.nextInt();
-        int resultado = Hasta100(edad);
+        int resultado = hasta100(edad);
         if(edad<0){
             System.out.println("Edad no válida");
         }else{
             if(edad>=100){
-                int pasado100 = Desde100(edad);
+                int pasado100 = desde100(edad);
                 System.out.printf("Ya has llegado a los 100 años, tienes %d años más de 100", pasado100);
             }else{
                 System.out.printf("Te faltan %d años para llegar a los 100.", resultado);
