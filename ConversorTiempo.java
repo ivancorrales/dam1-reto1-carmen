fb30b6dcf62b1ca8a7c314c4d80e3cbaff0587d8
Iván <ivan.corrales@wesovilabs.com>
2025-10-21 05:16:52 +0200

    Simplify the code

--- ConversorTiempo.java
+++ ConversorTiempo.java
@@ -1,34 +1,34 @@
 public class ConversorTiempo {
-    public static void conversor(int minutos){
-        int resultadoHoras = minutos/60;
-        int resultadoMinutos = minutos%60;
-        if(resultadoHoras == 1){
-            if(resultadoMinutos == 1){
-                System.out.printf("%d minutos equivalen a %d hora y %d minuto", minutos, resultadoHoras, resultadoMinutos);
-            }else{
-                System.out.printf("%d minutos equivalen a %d hora y %d minutos", minutos, resultadoHoras, resultadoMinutos);
-            }
-        }else{
-            if(resultadoMinutos == 1){
-                System.out.printf("%d minutos equivalen a %d horas y %d minuto", minutos, resultadoHoras, resultadoMinutos);
-            }else{
-                System.out.printf("%d minutos equivalen a %d horas y %d minutos%n", minutos, resultadoHoras, resultadoMinutos);
-            }
+
+    private static void mostrarHorasYMinutos(int input, int horas, int minutos) {
+        String textHoras = "hora";
+        if (horas!=1) {
+            textHoras = "horas";
         }
-    }
-    public static void conversor(int horas, int minutos){
-        if(minutos > 59){
-            int conversion = minutos/60;
-            int resultadoMinutos = minutos%60;
-            horas = horas + conversion;
-            System.out.printf("El resultado normalizado de %d horas y %d minutos es %d horas y %d minutos", (horas - conversion), minutos, horas, resultadoMinutos);
-        }else{
-            System.out.println("No se puede normalizar ya que los hay menos de 59 minutos");
+        String textMinuto = "minuto";
+        if (minutos!=1) {
+            textMinuto = "minutos";
         }
+        System.out.printf("%d minutos equivalen a %d %s y %d %s.%n", input, horas, textHoras, minutos, textMinuto);
     }
+
+    public static void conversor(int minutos) {
+        conversor(0,minutos);
+    }
+
+    public static void conversor(int horas, int minutos) {
+        int conversion = minutos / 60;
+        int resultadoMinutos = minutos % 60;
+        horas = horas + conversion;
+        mostrarHorasYMinutos(minutos, horas, resultadoMinutos);
+    }
+
     public static void main(String[] args) {
         conversor(2);
+        conversor(62);
         conversor(1, 70);
+        conversor(0, 61);
+
     }
 
 }
